88185f69748708b16132ef2b155015f29167d7d9
orly-chanella <orly_chanella.irumva@edu.univ-fcomte.fr>
2024-04-11 16:40:25 +0200

    ajout de la transition

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,21 @@ onErrorCaptured((err, instance, info) => {
   menu
 </button>
 <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+<Transition
+  class="transition-transform duration-1000"
+  enter-from-class="-translate-x-full"
+  enter-to-class="translate-x-0"
+  leave-active-class="-translate-x-full">
 <nav id="mainNav" v-show="menuIsOpen">
+
   <ul>
     <li><a href="#">item 1</a></li>
     <li><a href="#">item 2</a></li>
     <li><a href="#">item 3</a></li>
   </ul>
 </nav>
+</Transition>
+
     <nav>
       <ul>
         <li>
