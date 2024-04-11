9e269ada442c7f8afc295eecc10bed47f52f49a6
orly-chanella <orly_chanella.irumva@edu.univ-fcomte.fr>
2024-04-11 16:51:13 +0200

    ajout accordéon

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,16 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed quam quas eos quisquam deleniti aliquid temporibus! Ea earum quam ipsa libero excepturi blanditiis dolorum architecto omnis, doloremque doloribus culpa! Nisi?</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Sequi quo reprehenderit repellendus molestias, corporis, magnam optio ad, odio ipsam dolorem facilis? Earum magnam dicta sequi necessitatibus tenetur odit, minima animi nihil! Voluptatibus enim fugiat eos, error facilis eius?</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Impedit deserunt quaerat ex, perferendis omnis consectetur repellendus non. Quod obcaecati vitae nam quos aliquam voluptatem vero voluptates aut enim ullam? Voluptas perferendis sed similique, pariatur ab quaerat magni eum?</p>
+  </section>
 </template>
